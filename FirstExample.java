3b6aa8af61af2ea49ba479d608ece9e906116400
teacher Marcos <teacher.mrdv@gmail.com>
2020-09-28 19:29:07 +0800

    basics

---
+++ FirstExample.java
@@ -0,0 +1,16 @@
+// IBIO <- we use this to abbreviate 
+public class FirstExample
+{
+	
+	public static void main (String[] args)
+	{
+		// output example
+		IBIO.output("");
+		IBIO.output("Hello");
+		IBIO.output("Grade 11s!");
+		IBIO.out("What ");
+		IBIO.out("is your name");
+		IBIO.out("?");
+	}
+}
+
--- Template.java
+++ Template.java
@@ -7,7 +7,7 @@ public class Template
 	// single line comment
 	public static void main (String[] args) 
 	{
-		
+		// your code goes here!
 	}
 }
 
