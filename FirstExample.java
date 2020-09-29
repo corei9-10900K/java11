aa5c9e11568cdd23f80b6ba28196e24b47f08a80
teacher Marcos <teacher.mrdv@gmail.com>
2020-09-29 12:25:22 +0800

    Update FirstExample.java

--- FirstExample.java
+++ FirstExample.java
@@ -1,16 +1,15 @@
 // IBIO <- we use this to abbreviate 
 public class FirstExample
 {
-	
 	public static void main (String[] args)
 	{
 		// output example
-		IBIO.output("");
-		IBIO.output("Hello");
+		IBIO.output("\nHello");
 		IBIO.output("Grade 11s!");
-		IBIO.out("What ");
+		IBIO.out("What\t");
 		IBIO.out("is your name");
 		IBIO.out("?");
+		IBIO.output("\nI'm fine!");
 	}
 }
 
