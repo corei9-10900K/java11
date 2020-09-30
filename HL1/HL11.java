caf06a2de32fbd40200954d1f8d865706a490ad5
teacher Marcos <teacher.mrdv@gmail.com>
2020-09-30 14:06:20 +0800

    Update HL11.java

--- HL1/HL11.java
+++ HL1/HL11.java
@@ -24,15 +24,57 @@ public class HL11
 		}
 	}
 	
+	static int fibonacci(int t)
+	{
+		c = 0;
+		int previous = 0;
+		int next = 1;
+		int sum = 0;
+		for(int i = 2; i <= t; i++) // i = i + 1
+		{
+			sum = previous + next;
+			previous = next;
+			next = sum;
+			c = c + 1;
+		}
+		return sum;
+	}
+	
+	static int fact(int n)
+	{
+		c = 0;
+		int f = 1;
+		for(int i = 2; i <= n; i++)
+		{
+			c++;
+			f = f * i;
+		}
+		return f;
+	}
+	
+	
+	
 	// DO THE FOLLOWING
 	public static void main(String[] args)
 	{
+		c = 0;
+		System.out.println();
+		System.out.println("\"Hello\"");
+		System.out.println( "\n8! = " + fact(8) );
+		System.out.println( c + " loops\n" );
+		
+		c = 0;
+		System.out.println();
+		System.out.println("\"Hello\"");
 		System.out.println( "\n8! = " + factorial(8) );
 		System.out.println( c + " recursive calls\n" );
 		c = 0;
 		
-		System.out.println( "Fibonacci(8) = " + fibo(8) );
+		System.out.println( "Fibonacci(8) = " + fibo(30) );
 		System.out.println( c  + " recursive calls" );
+		
+		System.out.println( "Fibonacci(8) = " + fibonacci(30) );
+		System.out.println( c  + " loops" );
 		
 	}
 }
