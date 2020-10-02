0d78fdab09919418d806e23bbc7c7cdc4725a619
teacher Marcos <teacher.mrdv@gmail.com>
2020-10-02 19:02:47 +0800

    Update IfExamples.java

--- IfExamples.java
+++ IfExamples.java
@@ -1,10 +1,22 @@
-
+/*
+ * Demonstrates the use of IF in Java
+ * 
+ * The list below shows the binary (you can only compare two items at one time) operators:
+ * 
+ * && means AND
+ * || means OR
+ * <, <=, ==, !=, >, >=
+ * == compares it the two elements are the same or not
+ * != means not equal to
+ * 
+ */
 public class IfExamples
 {
 	
 	public static void main (String[] args)
 	{
 		int number = IBIO.inputInt("Enter a number: ");
+		System.out.println(number != 666 && number > 0)
 		if(number != 666 && number > 0)
 		{
 			IBIO.output("Holy numbers, Batman!");
