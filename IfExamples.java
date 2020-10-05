fa3c5f402e6f3af4086d7e4b649c091e7d1642ee
teacher Marcos <teacher.mrdv@gmail.com>
2020-10-05 13:04:32 +0800

    up

--- IfExamples.java
+++ IfExamples.java
@@ -16,7 +16,7 @@ public class IfExamples
 	public static void main (String[] args)
 	{
 		int number = IBIO.inputInt("Enter a number: ");
-		System.out.println(number != 666 && number > 0)
+		System.out.println(number != 666 && number > 0);
 		if(number != 666 && number > 0)
 		{
 			IBIO.output("Holy numbers, Batman!");
@@ -24,7 +24,7 @@ public class IfExamples
 		if(number == 666 || number < 0)
 		{
 			IBIO.output("Wicked.");
-		}
+		
 		if(number < 0)
 		{
 			IBIO.output("Don't be so negative.");
